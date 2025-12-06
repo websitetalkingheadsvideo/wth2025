c8d5c77b37ca40ee4e845d1a166d8447b4db6110
websitetalkingheadsvideo <websitetalkingheadsvideo@gmail.com>
2025-12-06 16:40:19 -0700

    chore: align contact page footer and header css

---
+++ contact-us/index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<title>Contact Website Talking Heads®</title>
+<meta name="keywords" content="web video, HTML5, video, online spokesperson, video spokesperson, website talking heads, website actor, website video, transparent video, virtual spokesperson, spokesperson, video presenter, website presenter, website spokesperson, video salesperson">
+<meta name="description" content="Website Talking Heads® creates Website Spokesperson, Presentations, Whiteboard and Animation Videos. We have more than a decade of experience in web video.">
+<meta name="robots" content="index, follow">
+<meta name="revisit-after" content="30 days">
+<meta name="rating" content="general">
+<meta name="verify-v1" content="YNESpqoAwK51PmBV7/PFKLG0agx7AQPKhXXcYAXGGF8="/>
+<meta name="norton-safeweb-site-verification" content="iinbv24r-1ix20hgj5l94wz2rnn3aiwi0336hwysvvpiskquy6ijsh9wy12f3znbed-hz1ay8ppzhgqap-sicqtw6ui29d0wrfcpenudh1ml9xwjbej7u25xy9pnm6yr"/>
+<?php
+  include( "../includes/css-b4.php" );
+  header( "X-Frame-Options: DENY" );
+  header( "Content-Security-Policy: frame-ancestors 'none'", false );
+?>
+<link rel="stylesheet" href="/css/mobile.css">
+</head>
+
+<body>
+<?php include ('../includes/header19.php'); ?>
+<section class="jumbotron">
+  <div class="container">
+    <h1>Contact Us</h1>
+    <div class="row">
+      <div class="col-6 text-center">
+        <p>:: Mailing address ::</p>
+        <p>Website Talking Heads®<br>
+          245 W. 9000 S.<br>
+          Sandy, UT 84070<br>
+        </p>
+        <p>:: Phone ::<br>
+          1-801-748-2281
+        <p>
+        <p>:: Inquiries and Questions ::<br>
+          <a href="mailto:Sales@WebsiteTalkingHeads.com" title="Inquiries">Sales@WebsiteTalkingHeads.com</a></br>
+        </p>
+      </div>
+      <div class="col-6 mt-4">
+		  <div class="pipedriveWebForms" data-pd-webforms="https://webforms.pipedrive.com/f/6WfFZtxIYlaDFR4wTsVVlsIHtzxSXXyZyYRL3Dszh0VTEFZRPJOThyDuWpXMrwOaYP"><script src="https://webforms.pipedrive.com/f/loader"></script></div>
+      </div>
+    </div>
+  </div>
+</section>
+<hr>
+<?php include ('../includes/CallForQuote.php'); ?>
+<?php include ('../includes/footer_b4.php'); ?>
+</body>
+</html>
